50b3b6415b7394901b181b750c78500f62aeff4a
FaisalAkbar21 <faisalakbr@student.telkomuniversity.ac.id>
2022-03-05 17:58:46 +0700

    MOD2

---
+++ KPL_MOD2_1302190110/MOD2_1302190110/MOD2_1302190110/Form1.cs
@@ -0,0 +1,30 @@
+namespace MOD2_1302190110
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
